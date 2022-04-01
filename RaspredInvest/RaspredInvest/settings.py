ca37dffbd432acc4716b420824b1b86bee8ff8b5
Dima Korshunov <korsunov2000@mail.ru>
2022-04-01 20:34:47 +0300

    Newdeploy5

--- RaspredInvest/RaspredInvest/settings.py
+++ RaspredInvest/RaspredInvest/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 ROOT_URLCONF = 'RaspredInvest.urls'
 
@@ -119,7 +120,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
